60178a87f66e29b4414d81d547138ec665c17c56
pervezmia <ahmedpervezkabir@gmail.com>
2026-03-07 12:55:10 +0600

    all card show in browser by fetch api

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,9 @@
 const totalIssue = document.getElementById("total-issue");
-// const allBtn = document.getElementById("all-btn");
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+
+const cardContainer = document.getElementById("card-container");
 
  /* active btn */
 const activeBtn = (id) => {
@@ -16,3 +20,52 @@ const activeBtn = (id) => {
     
 }
 activeBtn("all-btn");
+
+/* all data fetch */
+
+const loadAllData = async () => {
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json();
+    console.log(data.data);
+    renderingAllData(data.data);
+}
+
+
+const renderingAllData = async (allData) => {
+    
+    allData.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "card bg-base-100 shadow-sm";
+        card.innerHTML = `
+                <div class="card bg-base-100 shadow-sm">
+
+                    <div class="card-body">
+                        <div class="flex justify-between items-center">
+                            <img src="./assets/Open-Status.png" alt="">
+                            <div class="badge badge-warning">
+                                ${item.priority}
+                            </div>
+                        </div>
+                        <h2 class="card-title">Fix navigation menu on mobile devices</h2>
+                        <p class="line-clamp-2">The navigation menu doesn't collapse properly on mobile devices The navigation menu doesn't collapse properly on mobile devices</p>
+                        <div class="flex flex-col md:flex-row sm:justify-start gap-2.5">
+                            <div class="badge badge-warning">
+                                Warning
+                            </div>
+                            <div class="badge badge-warning">
+                                Warning
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <hr class="text-base-300">
+                    <div class="card-body rounded-2xl">
+                        <p>#1 by john_doe</p>
+                        <p>07/03/2024</p>
+                    </div>
+                </div>
+        `
+        cardContainer.appendChild(card);
+    })
+}
+loadAllData();
